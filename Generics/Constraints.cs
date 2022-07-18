19a02dac03d56e434b76fd3e08cd84d53d0d5d6c
ppedvLukasK <lukask@ppedv.de>
2022-07-18 14:19:53 +0200

    Generics

---
+++ Generics/Constraints.cs
@@ -0,0 +1,39 @@
+﻿namespace Generics;
+
+internal class Constraints
+{
+	static void Main(string[] args)
+	{
+		DataStore4<TestClass> ds;
+	}
+
+	public class DataStore1<T> where T : class { } //Referenztyp erzwingen
+
+	public class DataStore2<T> where T : struct { } //Wertetyp erzwingen
+
+	public class DataStore3<T> where T : Constraints { } //Vererbungshierarchie erzwingen
+
+	public class DataStore4<T> where T : new() { } //Nur Typen die einen Default Konstruktor haben
+
+	public class DataStore5<T> where T : Enum { } //Nur Enums
+
+	public class DataStore6<T> where T : Delegate { } //Nur Delegates (Action, Predicate, Func, EventHandler, ...)
+
+	public class DataStore7<T> where T : unmanaged { } //Bestimmte Typen (z.B.: int, bool, double, ...) https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/unmanaged-types
+
+	public class DataStore8<T>
+		where T : class, new() //Mehrere Constraints (Ein Referenztyp mit zusätzlich Default Konstruktor)
+	{ }
+
+	public class DataStore9<T1, T2> //Mehrere Constraints auf mehrere Generics machen
+		where T1 : class
+		where T2 : struct
+	{ }
+}
+
+public class TestClass
+{
+	public TestClass(int i) { } //Eigener Konstruktor überschreibt default Konstruktor
+
+	public TestClass() { } //Default Konstruktor wiederherstellen
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+
+namespace Generics;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		List<string> list = new List<string>(); //Generic: T wird nach unten übernommen (hier T = string)
+		list.Add("Max"); //T wird durch string ersetzt: Add(T) -> Add(string)
+
+		Dictionary<string, int> dictionary = new Dictionary<string, int>(); //Klasse mit 2 Generics: TKey (string), TValue (int)
+		dictionary.Add("Max", 1); //string als TKey, int als TValue: Add(TKey, TValue) -> Add(string, int)
+
+		foreach (KeyValuePair<string, int> kv in dictionary) { } //Dictionary iterieren
+	}
+}
+
+public class DataStore<T> //Beim Erstellen eines Objekts muss ein Generic angegeben werden
+	: IProgress<T>, //T bei Vererbung übergeben
+	  IEnumerable<int>
+{
+	private T[] data = new T[10]; //Array vom Typ T
+
+	public List<T> Data => data.ToList(); //T nach unten in ein weiteres Generic übergeben
+
+	public void Add(int index, T item) //Generic in Methodenparameter
+	{
+		data[index] = item;
+	}
+
+	public T GetIndex(int index) //T als Return Wert
+	{
+		if (index < 0 || index >= data.Length)
+			return default(T); //default(T): Standardwert von T (int: 0, string: null, bool: false, Program: null)
+		return data[index];
+	}
+
+	public void PrintType<MyType>() //Generic in Methode
+	{
+		Console.WriteLine(typeof(MyType));
+		Console.WriteLine(nameof(MyType)); //MyType als string: "MyType"
+		Console.WriteLine(default(MyType));
+	}
+
+	public void Report(T value) //T wird von oben weitergegeben
+	{
+		throw new NotImplementedException();
+	}
+
+	IEnumerator IEnumerable.GetEnumerator() => throw new NotImplementedException();
+
+	public IEnumerator<int> GetEnumerator() //Oben bei Vererbung fixen Typ übergeben (hier int)
+	{
+		throw new NotImplementedException();
+	}
+}
+
+public class DataStore2<T> : DataStore<T> //Klassen mit T vererben: braucht wieder T beim Klassennamen
+{
+
+}
