8c4cbfee706623413acdeb80a89daa3a290ee011
ppedvLukasK <lukask@ppedv.de>
2022-07-18 10:31:43 +0200

    CSharp7-9

---
+++ Test/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace Test
+{
+	internal record Person
+	(
+		[field: NonSerialized] int ID, //Attribut auf Record Property
+		[NonSerialized] string Name //nicht möglich ohne field:
+	)
+	{
+		//Code in Record hinzufügen
+
+		//Generiert auch Deconstruct, GetHashCode, Equals, ...
+	}
+}
---
+++ Test/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Test
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			ITest t;
+			//t.TestMethode();
+		}
+	}
+
+	interface ITest
+	{
+		public static int Etwas => 20; //OK
+
+		public void TestMethode()
+		{
+			//bad practice
+		}
+	}
+}
