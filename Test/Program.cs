0ad04d74b4fa1b50e5b42eb465e48f816259fc12
ppedvLukasK <lukask@ppedv.de>
2022-07-18 15:43:52 +0200

    Generics Lösung

---
+++ Test/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Test;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Node<SolarItem> node = new Node<Sun>();
+	}
+}
+
+public interface Node<out T>
+	where T : SolarItem
+{
+	//public T Self;
+
+	//public Node<T> Parent;
+
+	//public List<Node<T>> Children;
+}
+
+public class SolarItem { }
+
+public class Sun : SolarItem { }
+
+public class Planet : SolarItem { }
+
+public class Moon : SolarItem { }
