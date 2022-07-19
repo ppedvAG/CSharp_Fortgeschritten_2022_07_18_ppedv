85fcd73b235f10dc35470466f73cbfee34c9c55f
ppedvLukasK <lukask@ppedv.de>
2022-07-19 09:33:11 +0200

    Serialisierung

--- Serialisierung/Program.cs
+++ Serialisierung/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualBasic.FileIO;
-using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -13,37 +14,9 @@ internal class Program
 {
 	static void Main(string[] args)
 	{
-		string desktop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory); //Environment.GetFolderPath: Speziellen statischen Pfad holen
-
-		string folderPath = Path.Combine(desktop, "Serialisierung"); //Path.Combine: beliebig viele Pfade kombinieren
-
-		if (!Directory.Exists(folderPath))
-			Directory.CreateDirectory(folderPath);
-
-		string filePath = Path.Combine(folderPath, "Test.txt");
-
-		List<Fahrzeug> fahrzeuge = new()
-		{
-			new Fahrzeug(0, 251, FahrzeugMarke.BMW),
-			new Fahrzeug(1, 274, FahrzeugMarke.BMW),
-			new Fahrzeug(2, 146, FahrzeugMarke.BMW),
-			new Fahrzeug(3, 208, FahrzeugMarke.Audi),
-			new Fahrzeug(4, 189, FahrzeugMarke.Audi),
-			new Fahrzeug(5, 133, FahrzeugMarke.VW),
-			new Fahrzeug(6, 253, FahrzeugMarke.VW),
-			new Fahrzeug(7, 304, FahrzeugMarke.BMW),
-			new Fahrzeug(8, 151, FahrzeugMarke.VW),
-			new Fahrzeug(9, 250, FahrzeugMarke.VW),
-			new Fahrzeug(10, 217, FahrzeugMarke.Audi),
-			new Fahrzeug(11, 125, FahrzeugMarke.Audi)
-		};
-
 		//Json();
 
-		//Newtonsoft.Json
-		//JsonConvert
-		//[""] statt GetProperty
-		//.Value<T>() statt .Get<Typ>()
+		//NewtonsoftJson();
 
 		//Xml();
 
@@ -79,15 +52,15 @@ internal class Program
 			new Fahrzeug(11, 125, FahrzeugMarke.Audi)
 		};
 
-		JsonSerializerOptions options = new JsonSerializerOptions() { WriteIndented = true }; //Einstellungen beim Serialisieren
+		JsonSerializerOptions options = new() { WriteIndented = true }; //Einstellungen beim Serialisieren
 
 		using StreamWriter sw = new(filePath) { AutoFlush = true };
-		string json = JsonSerializer.Serialize(fahrzeuge, options); //Objekt serialisieren, WICHTIG: Alle Felder müssen Properties sein
+		string json = System.Text.Json.JsonSerializer.Serialize(fahrzeuge, options); //Objekt serialisieren, WICHTIG: Alle Felder müssen Properties sein
 		sw.WriteLine(json);
 		sw.Close();
 
 		using StreamReader sr = new(filePath);
-		List<Fahrzeug> readFahrzeuge = JsonSerializer.Deserialize<List<Fahrzeug>>(sr.BaseStream); //Fahrzeuge von Json String zu Liste konvertieren
+		List<Fahrzeug> readFahrzeuge = System.Text.Json.JsonSerializer.Deserialize<List<Fahrzeug>>(sr.BaseStream); //Fahrzeuge von Json String zu Liste konvertieren
 
 		JsonDocument doc = JsonDocument.Parse(json); //Json String zu JsonDocument umwandeln um damit zu arbeiten
 		JsonElement root = doc.RootElement; //Oberstes Element (Baumstruktur)
@@ -98,6 +71,51 @@ internal class Program
 
 			Fahrzeug fzg = e.Deserialize<Fahrzeug>(); //Einzelnes Fahrzeug erstellen
 			Console.WriteLine(fzg.MaxGeschwindigkeit);
+		}
+	}
+
+	public static void NewtonsoftJson()
+	{
+		string desktop = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory); //Environment.GetFolderPath: Speziellen statischen Pfad holen
+
+		string folderPath = Path.Combine(desktop, "Serialisierung"); //Path.Combine: beliebig viele Pfade kombinieren
+
+		if (!Directory.Exists(folderPath))
+			Directory.CreateDirectory(folderPath);
+
+		string filePath = Path.Combine(folderPath, "Test.txt");
+
+		List<Fahrzeug> fahrzeuge = new()
+		{
+			new Fahrzeug(0, 251, FahrzeugMarke.BMW),
+			new Fahrzeug(1, 274, FahrzeugMarke.BMW),
+			new Fahrzeug(2, 146, FahrzeugMarke.BMW),
+			new Fahrzeug(3, 208, FahrzeugMarke.Audi),
+			new Fahrzeug(4, 189, FahrzeugMarke.Audi),
+			new Fahrzeug(5, 133, FahrzeugMarke.VW),
+			new Fahrzeug(6, 253, FahrzeugMarke.VW),
+			new Fahrzeug(7, 304, FahrzeugMarke.BMW),
+			new Fahrzeug(8, 151, FahrzeugMarke.VW),
+			new Fahrzeug(9, 250, FahrzeugMarke.VW),
+			new Fahrzeug(10, 217, FahrzeugMarke.Audi),
+			new Fahrzeug(11, 125, FahrzeugMarke.Audi)
+		};
+
+		JsonSerializerSettings setting = new() { Formatting = Newtonsoft.Json.Formatting.Indented, TypeNameHandling = TypeNameHandling.Objects };
+
+		string json = JsonConvert.SerializeObject(fahrzeuge, Newtonsoft.Json.Formatting.Indented);
+		File.WriteAllText(filePath, json);
+
+		string readJson = File.ReadAllText(filePath);
+		List<Fahrzeug> parsedJson = JsonConvert.DeserializeObject<List<Fahrzeug>>(readJson);
+
+		JToken jt = JToken.Parse(readJson);
+		foreach (JToken fzg in jt) //Children durchgehen
+		{
+			Console.WriteLine(fzg["MaxGeschwindigkeit"].Value<int>()); //Auf Felder in Children zugreifen mit [], Value<T> zum casten
+
+			Fahrzeug f = JsonConvert.DeserializeObject<Fahrzeug>(fzg.ToString()); //JToken Deserialisieren über ToString()
+			Console.WriteLine(f.MaxGeschwindigkeit);
 		}
 	}
 
@@ -128,7 +146,7 @@ internal class Program
 			new Fahrzeug(11, 125, FahrzeugMarke.Audi)
 		};
 
-		XmlSerializer xmlS = new XmlSerializer(typeof(List<Fahrzeug>)); //In Klammer den Typ angeben (hier List<Fahrzeug> statt Fahrzeug)
+		XmlSerializer xmlS = new XmlSerializer(fahrzeuge.GetType()); //In Klammer den Typ angeben (hier List<Fahrzeug> statt Fahrzeug), typeof(List<Fahrzeug>)
 		using FileStream fs = new FileStream(filePath, FileMode.Create); //FileStream mit Create -> Überschreiben
 		xmlS.Serialize(fs, fahrzeuge);
 
@@ -179,7 +197,7 @@ internal class Program
 
 		string[] headers = tfp.ReadFields();
 
-		List<Fahrzeug> lines = new();
+		List<Fahrzeug> fzgs = new();
 		while (!tfp.EndOfData)
 		{
 			Fahrzeug f = new();
@@ -187,7 +205,7 @@ internal class Program
 			f.ID = int.Parse(fields[0]);
 			f.MaxGeschwindigkeit = int.Parse(fields[1]);
 			f.Marke = (FahrzeugMarke) int.Parse(fields[2]);
-			lines.Add(f);
+			fzgs.Add(f);
 		}
 	}
 
@@ -235,9 +253,11 @@ public class Fahrzeug
 	public int ID { get; set; }
 
 	[JsonPropertyName("MaxV")] //Name vom Feld im Json angeben
+	[JsonProperty("MaxV")] //Newtonsoft Json Equivalent zu JsonPropertyName
 	public int MaxGeschwindigkeit { get; set; }
 
-	[JsonIgnore] //Feld beim Serialisieren ignorieren
+	[System.Text.Json.Serialization.JsonIgnore] //Feld beim Serialisieren ignorieren
+	[Newtonsoft.Json.JsonIgnore]
 	public FahrzeugMarke Marke { get; set; }
 
 	public Fahrzeug(int id, int v, FahrzeugMarke fm)
